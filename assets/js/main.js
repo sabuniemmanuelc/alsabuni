8aca20d3b56f42a5fe43d282a800c8e6d37fe195
Sabuni Emmanuel C <sabuni.emmanuelc@gmail.com>
2025-06-15 14:30:36 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,7 @@ function sendMail() {
         document.getElementById("email").value = "";
         document.getElementById("message").value = "";
         console.log(res);
-        alert("Your message sent successfully!!")
+        alert("Your message sent successfully!!");
 
     })
     .catch(err=>console.log(err));
