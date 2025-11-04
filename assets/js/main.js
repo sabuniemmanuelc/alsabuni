0a6b892dd457605d98cca4d1f40897dafa99850f
Sabuni Emmanuel C <sabuni.emmanuelc@gmail.com>
2025-11-04 16:26:07 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,25 +8,29 @@
 
  })
 
- function sendMail() {
+ // Add this to your existing script
+document.getElementById('contact-form').addEventListener('submit', sendMail);
+
+function sendMail(event) {
+  event.preventDefault();
+  
   var params = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
-    project: document.getElementById("message").value,
+    message: document.getElementById("message").value,
   };
 
   const serviceID = "service_8xt0rlq";
   const templateID = "template_ym9ghe3";
 
-    emailjs.send(serviceID, templateID, params)
-    .then(res=>{
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";
-        console.log(res);
-        alert("Your message sent successfully!!")
-
+  emailjs.send(serviceID, templateID, params)
+    .then(res => {
+      document.getElementById("contact-form").reset();
+      console.log(res);
+      alert("Your message sent successfully!!");
     })
-    .catch(err=>console.log(err));
-
+    .catch(err => {
+      console.log(err);
+      alert("Failed to send message. Please try again.");
+    });
 }
